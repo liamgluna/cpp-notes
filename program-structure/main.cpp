c9736bb9608bde1045367d0d3d83ad970324e614
liamgluna <74529953+liamgluna@users.noreply.github.com>
2023-02-12 16:38:32 +0800

    documentation edits

--- program-structure/main.cpp
+++ program-structure/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
  * 
- * @brief How to structure your project to have a separate file for your classes implementation(.cpp) and interface(.hpp file).
+ * @brief Separate class implementation (.cpp) and interface (.hpp) files in your project structure.
  * 
  * @author
  *  Liam Larry G. Luna <liamlarry.luna@lsu.edu.ph>
