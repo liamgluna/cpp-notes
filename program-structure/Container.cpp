e153cd0356f5c02040e2753c6a54e999d9082f71
liamgluna <74529953+liamgluna@users.noreply.github.com>
2023-02-12 16:35:13 +0800

    program structure edits

--- program-structure/Container.cpp
+++ program-structure/Container.cpp
@@ -1,6 +1,8 @@
 /**
- * How to structure your project to have a separate file for your classes implementation(.cpp) and interface(.hpp file).
  * @file Container.cpp (Implementation)
+ * 
+ * @brief How to structure your project to have a separate file for your classes implementation(.cpp) and interface(.hpp file).
+ * 
  * @author
  *  Liam Larry G. Luna <liamlarry.luna@lsu.edu.ph>
  */
